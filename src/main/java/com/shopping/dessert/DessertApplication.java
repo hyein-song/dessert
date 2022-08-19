7a6536deb41efbe8408e0766876a88b64bcda148
hyein-song <shi9512@naver.com>
2022-08-20 00:23:05 +0900

    Initial commit

---
+++ src/main/java/com/shopping/dessert/DessertApplication.java
@@ -0,0 +1,13 @@
+package com.shopping.dessert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DessertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DessertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopping/dessert/DessertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopping.dessert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DessertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
