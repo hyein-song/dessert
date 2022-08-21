a72a5e05bf0a0afac3f167918445b001ee9d4142
hyein-song <shi9512@naver.com>
2022-08-21 23:53:48 +0900

    repository 생성

---
+++ src/main/java/com/shopping/dessert/repository/CartRepository.java
@@ -0,0 +1,9 @@
+package com.shopping.dessert.repository;
+
+import com.shopping.dessert.entity.CartEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends JpaRepository<CartEntity, Long> {
+}
---
+++ src/main/java/com/shopping/dessert/repository/OrderProductRepository.java
@@ -0,0 +1,9 @@
+package com.shopping.dessert.repository;
+
+import com.shopping.dessert.entity.OrderProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderProductRepository extends JpaRepository<OrderProductEntity, Long> {
+}
