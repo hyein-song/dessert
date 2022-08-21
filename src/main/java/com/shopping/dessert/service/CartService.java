9694c46ef0de1afe6bbc0c301c11c57ea0377703
hyein-song <shi9512@naver.com>
2022-08-21 23:57:25 +0900

    service 생성

---
+++ src/main/java/com/shopping/dessert/service/CartService.java
@@ -0,0 +1,9 @@
+package com.shopping.dessert.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CartService {
+}
