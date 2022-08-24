447ea5ed4a1796c588753bd9f5327a30c8e3c9f1
hyein-song <shi9512@naver.com>
2022-08-25 01:13:32 +0900

    test controller

--- src/main/java/com/shopping/dessert/controller/HomeController.java
+++ src/main/java/com/shopping/dessert/controller/HomeController.java
@@ -1,18 +1,18 @@
 package com.shopping.dessert.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("")
-    public String home(Model model){
-
-        // TODO: main에 보여줄 product 추가
-
-
+    public String home(){
         return "home";
+    }
+
+    @GetMapping("/detail")
+    public String detail(){
+        return "detail";
     }
 }
