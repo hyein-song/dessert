999c3b8d95032901b0937da74c708fd37684257d
hyein-song <shi9512@naver.com>
2022-09-03 04:36:58 +0900

    product detail에서 상품 수량 변경 기능 - html 내에 script 작성

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -85,41 +85,21 @@
 
 
     // Product Quantity
-    $('.quantity button').on('click', function () {
-        var button = $(this);
-        var oldValue = button.parent().parent().find('input').val();
-        if (button.hasClass('btn-plus')) {
-            var newVal = parseFloat(oldValue) + 1;
-        } else {
-            if (oldValue > 0) {
-                var newVal = parseFloat(oldValue) - 1;
-            } else {
-                newVal = 0;
-            }
-        }
-        button.parent().parent().find('input').val(newVal);
-    });
-    //
-    // function changeQuantity{
-    //     $('.quantity button').on('click', function () {
-    //         console.log("버튼 클릭")
-    //         var button = $(this);
-    //         var oldValue = button.parent().parent().find('input').val();
-    //         if (button.hasClass('btn-plus')) {
-    //             var newVal = parseFloat(oldValue) + 1;
+    // $('.quantity button').on('click', function () {
+    //     var button = $(this);
+    //     var oldValue = button.parent().parent().find('input').val();
+    //     if (button.hasClass('btn-plus')) {
+    //         var newVal = parseFloat(oldValue) + 1;
+    //     } else {
+    //         if (oldValue > 0) {
+    //             var newVal = parseFloat(oldValue) - 1;
     //         } else {
-    //             if (oldValue > 0) {
-    //                 var newVal = parseFloat(oldValue) - 1;
-    //             } else {
-    //                 newVal = 0;
-    //             }
+    //             newVal = 0;
     //         }
-    //         button.parent().parent().find('input').val(newVal);
-    //     });
-    // }
+    //     }
+    //     button.parent().parent().find('input').val(newVal);
+    // });
 
 
-
-    
 })(jQuery);
 
